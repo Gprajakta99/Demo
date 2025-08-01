ba4cb83f4855a47ee2fd68b6bf5e77bb871e198a
Gprajakta99 <gprajakta81@gmail.com>
2025-08-01 23:55:07 +0530

    second commit

---
+++ server.js
@@ -0,0 +1,65 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js"; // note the .js extension
+import cors from "cors";
+import User from "./model/user.js"; // make sure .js is used here too
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+app.post("/users", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true } // Return updated user
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
